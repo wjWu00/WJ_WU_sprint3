27edbf462eed61c622a434a1d9f82b0d679bc6b0
wwj0000 <71466855+wwj0000@users.noreply.github.com>
2021-02-28 11:56:01 -0500

    commit sprint3

---
+++ main.py
@@ -0,0 +1,95 @@
+import requests
+import secrets
+import sqlite3
+from typing import Tuple
+
+def get_data():
+    all_data = []
+    response = requests.get(f'https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant='
+                            f'2,3&fields=id,school.state,school.name,2018.student.size,2016.repayment.3_yr_repayment.overall,'
+                            f'2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line&api_key={secrets.api_key}')
+    first_page = response.json()
+    if response.status_code != 200:
+        print(F"Error Getting Data from API: {response.raw}")
+        return []
+    total_results = first_page['metadata']['total']
+    page = 0
+    per_page = first_page['metadata']['per_page']
+    all_data.extend(first_page['results'])
+    while (page+1)*per_page < total_results:
+        page += 1
+        response = requests.get(
+            f'https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.'
+            f'state,school.name,2018.student.size,2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.'
+            f'overall_count_over_poverty_line&api_key={secrets.api_key}&page={page}')
+        if response.status_code != 200:  # if we didn't get good data keep going
+            continue
+        current_page = response.json()
+        all_data.extend(current_page['results'])
+
+    return all_data
+
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)
+    cursor = db_connection.cursor()
+    return db_connection, cursor
+
+
+def close_db(connection: sqlite3.Connection):
+    connection.commit()
+    connection.close()
+
+
+def setup_schools_table(cursor: sqlite3.Cursor):
+    cursor.execute('''DROP TABLE IF EXISTS  schools''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS schools(
+    school_id INTEGER PRIMARY KEY,
+    school_name TEXT NOT NULL,
+    school_city TEXT NOT NULL,
+    student_size_2018 INTEGER,
+    student_size_2017 INTEGER,
+    earnings_2017 INTEGER,
+    repayment_2016 INTEGER
+    );''')
+
+
+def setup_state_table(cursor: sqlite3.Cursor):
+    cursor.execute('''DROP TABLE IF EXISTS  state_employment''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS state_employment( 
+    state_name TEXT NOT NULL PRIMARY KEY,
+    occupation_major TEXT NOT NULL,
+    total_emplpyment INTEGER,
+    25th_percentile_salary_hourly INTEGER,
+    25th_percentile_salary_annual INTEGER,
+    occupation_code TEXT NOT NULL
+    );''')
+
+
+def insert_schools_data(all_data, cursor):
+    for univ_data in all_data:
+        cursor.execute("""INSERT INTO SCHOOLS(school_id, school_name,school_state,student_size_2018,student_size_2017,
+        earnings_2017,repayment_2016)
+         VALUES (?,?,?,?,?,?,?);
+        """, (univ_data['id'], univ_data['school.name'], univ_data['school.state'], univ_data['2018.student.size'], univ_data['2017.student.size'],
+              univ_data['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'],
+              univ_data['2016.repayment.3_yr_repayment.overall']))
+
+def insert_employment_data(sheet, cursor):
+    for employment_data in sheet:
+        cursor.execute("""INSERT INTO EMPLOYMENT(state_name, occupation_major,total_emplpyment, 25th_percentile_salary_hourly, 25th_percentile_salary_annual,
+        occupation_code)
+        VALUES (?,?,?,?,?,?);""", (employment_data['state_name'], employment_data['occupation_major'],employment_data['total_emplpyment'],
+                                   employment_data['25th_percentile_salary_hourly'], employment_data['25th_percentile_salary_annual'],
+                                   employment_data['occupation_code']))
+
+
+def main():
+    all_data = get_data()
+    conn, cursor = open_db("college_data.sqlite")
+    setup_schools_table(cursor)
+    setup_state_table(cursor)
+    close_db(conn)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test.py
@@ -0,0 +1,30 @@
+import main
+
+# get_data will get a total of 3203 school entries
+def test_get_data():
+    result = main.get_data()
+    assert len(result) == 3203
+
+
+# test the database file
+def test_setup_db():
+    # add test data
+    conn, cursor = main.open_db("testdb.sqlite")
+    main.setup_db(cursor)
+    test_data = [{'school.id': 'Test University', '2018.student.size': 1000, 'school.state': 'MA', 'id': 11001,
+                  '2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line': 456,
+                  '2016.repayment.3_yr_repayment.overall': 4004}]
+    main.insert_data(test_data,cursor)
+    main.close_db(conn)
+    # test data is saved to see if it is there
+    conn,cursor = main.open_db('testdb.sqlite')
+    cursor.execute()
+    cursor.execute('''SELECT name FROM sqlite_master
+    WHERE type ='table' AND name LIKE 'university_%';''')  # like does pattern matching with % as the wildcard
+    results = cursor.fetchall()
+    assert len(results) == 1
+    cursor.execute(''' SELECT university_name FROM university_data''')
+    results = cursor.fetchall()
+    test_record = results[0]
+    assert test_record[0] == 'Test University'
+
